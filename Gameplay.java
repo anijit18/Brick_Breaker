fca0b4b86bc3aa639c7c4ce76f9502aa5e98d6b0
anijit18 <anijitpandey2002@gmail.com>
2023-05-07 16:08:08 +0530

    extra comments removed

--- Gameplay.java
+++ Gameplay.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.ActionListener;
-//import java.util.Timer;
 import javax.swing.Timer;
 
 public class Gameplay extends JPanel implements KeyListener, ActionListener {
